27a7f85b5e8cb72a263cfb58a97ac8c288aaa2d3
Natalia Titova <bnmlucky@gmail.com>
2023-12-01 10:30:37 -0800

    solutions for Add Binary problem in js and ts

---
+++ leetcode_easy/add_binary.ts
@@ -0,0 +1,48 @@
+function addBinary(a: string, b: string): string {
+    let result = '';
+    let carry = 0;
+
+    // Iterate from the rightmost digit to the leftmost digit
+    for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0 || carry > 0; i--, j--) {
+        const digitA = i >= 0 ? parseInt(a[i]) : 0;
+        const digitB = j >= 0 ? parseInt(b[j]) : 0;
+
+        // Sum the current digits and the carry
+        const sum = digitA + digitB + carry;
+
+        // Calculate the current bit and update the carry
+        const currentBit = sum % 2;
+        carry = Math.floor(sum / 2);
+
+        // Prepend the current bit to the result
+        result = currentBit + result;
+    }
+
+    return result;
+}
+// Explanation:
+
+// The addBinary function takes two binary strings, a and b, as input and returns their sum as a binary string.
+
+// We initialize an empty string result to store the final sum and a variable carry to store the carry during addition.
+
+// We use a loop to iterate through the binary strings from right to left, starting from the least significant bit.
+
+// Inside the loop, we extract the current bits (digitA and digitB) from the input strings a and b. If we reach the end of either string, we assume a zero for that string.
+
+// We sum the current bits (digitA, digitB) and the carry (carry).
+
+// We calculate the current bit of the sum (currentBit) and update the carry for the next iteration.
+
+// We prepend the current bit to the result string.
+
+// After the loop, if there's a remaining carry, we prepend it to the result.
+
+// The final result is the binary sum of the input strings.
+
+// This TypeScript solution is essentially the same as the JavaScript solution, with the addition of type annotations for the function parameters (a and b). TypeScript allows for static typing, which can enhance code readability and catch potential errors during development. The time complexity is O(max(N, M)), where N and M are the lengths of the input strings a and b.
+
+
+
+
+
