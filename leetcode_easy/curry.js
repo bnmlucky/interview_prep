7ada04d930f4d50adc7481a927915997aab88fbe
Natalia Titova <bnmlucky@gmail.com>
2024-03-05 10:01:43 -0800

    solutions to 3 problems in js

---
+++ leetcode_easy/curry.js
@@ -0,0 +1,8 @@
+var curry = function (fn) {
+    return function curried(...args) {
+        if (args.length >= fn.length) {
+            return fn.apply(this, args);
+        }
+        return curried.bind(this, ...args);
+    };
+};
